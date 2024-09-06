6c396d834172b1f5374cd7e7df63bfa26326d16c
Hapyto4y <joedanik@gmail.com>
2024-09-06 10:30:54 -0700

    Restart Function added
    game_restart function was added into the play_game function

--- Hangman.py
+++ Hangman.py
@@ -1,31 +1,38 @@
-import random
-word_list = ['performer', 'crackpot', 'combination', 'expertise', 'mourning', 'secretary', 'broccoli', 'health','outfit', 'timber', 'receipt', 'star', 'domestic', 'large', 'irony', 'root', 'building', 'laser','far', 'swim', 'lunch', 'budge', 'acceptable', 'tiptoe', 'museum', 'familiar', 'science', 'corruption','enlarge', 'musical', 'reliable', 'bite', 'rest', 'heel', 'atmosphere', 'secure', 'evoke', 'consolidate','modernize', 'joke', 'liability', 'enthusiasm', 'aspect', 'tendency', 'crime', 'hostile', 'orbit','ballet', 'clarify', 'stable']
-random_word = random.choice(word_list)
-wrd_holder = ' '.join("_" * len(random_word))
-# Create a string of underscores (with spaces in between for readability)
-# to represent the word
-list_holder = list(wrd_holder)
-# Convert the underscore string into a list for easier modification
+
+attempts_remaining = 10
 incorrect_guess_holder = []
-# To keep track of incorrect guesses
-
-attempts_remaining = 5
-# Number of guesses the user is allowed
-
 def play_game():
-    global attempts_remaining
-    global wrd_holder
-    global random_word
-    global list_holder
-    global incorrect_guess_holder
+    def game_restart():
+        global attempts_remaining
+        global incorrect_guess_holder
+        start_again = input("Do you want to play again?")
+        if start_again.lower() in ("yes", "y"):
+            attempts_remaining = 10
+            incorrect_guess_holder = []
+            play_game()
+        else:
+            raise SystemExit("Goodbye!")
+    import random
+    word_list = ['performer', 'crackpot', 'combination', 'expertise', 'mourning', 'secretary', 'broccoli', 'health','outfit', 'timber', 'receipt', 'star', 'domestic', 'large', 'irony', 'root', 'building', 'laser','far', 'swim', 'lunch', 'budge', 'acceptable', 'tiptoe', 'museum', 'familiar', 'science', 'corruption','enlarge', 'musical', 'reliable', 'bite', 'rest', 'heel', 'atmosphere', 'secure', 'evoke','consolidate', 'modernize', 'joke', 'liability', 'enthusiasm', 'aspect', 'tendency', 'crime','hostile', 'orbit', 'ballet', 'clarify', 'stable']
+    random_word = "joy"
+    #random_word = random.choice(word_list)
+    wrd_holder = ' '.join("_" * len(random_word))
+    # Create a string of underscores (with spaces in between for readability) to represent the word
 
+    list_holder = list(wrd_holder)
+    # Convert the underscore string into a list for easier modification
+
+    # To keep track of incorrect guesses
+    global attempts_remaining
+    global incorrect_guess_holder
+    # Number of guesses the user is allowed
     while attempts_remaining > 0:
         # Main game loop - keeps running until the user runs out of guesses and exits
         print(f"Current guess: {wrd_holder}")  # Show current state of the word to the user
         if not incorrect_guess_holder:
             pass #No current incorrect guesses
         else: print(f"Incorrect guesses: {incorrect_guess_holder}")
-        usr_input = input("Please guess a letter: ")
+        usr_input = input("Please guess a letter: ").lower()
 
         index = random_word.find(usr_input)
         # Initialize the index to search for the letter
@@ -43,18 +50,22 @@ def play_game():
 
             # Convert the list_holder back to a string to show progress to the user
             wrd_holder = ''.join(list_holder)
+            print("--------------------------------------")
+            print(f"{attempts_remaining} attempts left.")
+            print("--------------------------------------")
         else:
             incorrect_guess_holder.append(usr_input)
             # If the letter is not found, notify the user and decrement the attempts
+            print("--------------------------------------")
             print(f"Wrong letter! {attempts_remaining - 1} attempts left.")
+            print("--------------------------------------")
             attempts_remaining -= 1
+        won_game = wrd_holder.replace(" ", "")
+        if won_game == random_word:
+            print("\033[32mGame Won!\033[0m")
+            game_restart()
         if attempts_remaining == 0:
-            start_again = input("Do you want to play again?")
-            if start_again.lower() in ("yes", "y"):
-                attempts_remaining = 5
-                incorrect_guess_holder = []
-                play_game()
-            else: raise SystemExit("Goodbye!")
+            game_restart()
 
     print("Final word guess: ", wrd_holder)
     print(random_word)
